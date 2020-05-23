19d214553f2722a776dfb8398709fe4ec46a52c7
XMU_NB <1192310746@qq.com>
2020-05-23 17:47:54 +0800

    init repo

---
+++ src/test/java/cn/edu/xmu/MycommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.xmu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
